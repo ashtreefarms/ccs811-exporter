326fb5dfa5484311da4e43f8e3abe7995dccdccd
Jake Krog <jake.krog@mac.com>
2018-02-28 03:24:40 -0800

    Refactor exporter

--- ccs811_exporter/__main__.py
+++ ccs811_exporter/__main__.py
@@ -1,3 +1,4 @@
+from Adafruit_CCS811 import Adafruit_CCS811
 import argparse
 from ccs811_exporter import CCS811Exporter
 import json
@@ -5,34 +6,64 @@ import logging
 import prometheus_client
 from time import sleep
 
-DEFAULT_I2C_ADDRESS = 0x5B
-DEFAULT_PORT = 9501
-DEFAULT_SAMPLE_INTERVAL = 2
+_DEFAULT_I2C_ADDRESS = 0x5B
+_DEFAULT_PORT = 9501
+_DEFAULT_SAMPLE_INTERVAL = 2
+_DEFAULT_TEMP_OFFSET = -25.0
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity", action='store_true')
-parser.add_argument("-p", "--port", type=int, default=DEFAULT_PORT, help="exporter port")
-parser.add_argument("-a", "--address", type=lambda x: int(x, 0), default=DEFAULT_I2C_ADDRESS, help="CCS811 I2C address")
-parser.add_argument("-l", "--labels", type=json.loads, help="JSON object of Prometheus labels to apply")
-parser.add_argument("-i", "--interval", type=int, default=DEFAULT_SAMPLE_INTERVAL, help="measurement sample interval")
+logger = logging.getLogger('ccs811_exporter')
 
 
-def main(args=None):
-    if args is None:
-        args = parser.parse_args()
+def _init_arg_parser():
+    p = argparse.ArgumentParser()
+    p.add_argument("-v", "--verbose", action='store_true',
+                   help="increase output verbosity")
+    p.add_argument("-p", "--port",
+                   type=int, default=_DEFAULT_PORT,
+                   help="exporter port (default: {})"
+                        .format(_DEFAULT_PORT))
+    p.add_argument("-a", "--address",
+                   type=lambda x: int(x, 0), default=_DEFAULT_I2C_ADDRESS,
+                   help="CCS811 I2C address (default: 0x{:02x}"
+                        .format(_DEFAULT_I2C_ADDRESS))
+    p.add_argument("-l", "--labels", type=json.loads,
+                   help="JSON object of Prometheus labels to apply")
+    p.add_argument("-i", "--interval",
+                   type=int, default=_DEFAULT_SAMPLE_INTERVAL,
+                   help="measurement sample interval (default: {})"
+                        .format(_DEFAULT_SAMPLE_INTERVAL))
+    p.add_argument("-to", type=float, default=_DEFAULT_TEMP_OFFSET,
+                   help="temperature offset (default: {:.1f}"
+                        .format(_DEFAULT_TEMP_OFFSET))
+    return p
 
+
+def _configure_logger(args):
     f = logging.Formatter('%(asctime)s - %(message)s')
     ch = logging.StreamHandler()
     ch.setFormatter(f)
-    logger = logging.getLogger('ccs811_exporter')
     logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
     logger.addHandler(ch)
-    
-    exporter = CCS811Exporter(address=args.address, labels=args.labels)
+
+
+def main():
+    parser = _init_arg_parser()
+    args = parser.parse_args()
+
+    _configure_logger(args)
+
+    logger.info("initializing CCS811 at 0x{a:02x}".format(a=args.address))
+    ccs811 = Adafruit_CCS811(address=args.address)
+
+    logger.info("initializing CCS811 exporter with labels {}".format(args.labels))
+    exporter = CCS811Exporter(ccs811, labels=args.labels)
 
     logger.info("waiting for CCS811")
     exporter.wait()
-    exporter.set_temp_offset(-25.0)
+
+    logger.info("setting temperature offset to {:.1f}".format(args.to))
+    temp = ccs811.calculateTemperature()
+    ccs811.tempOffset = temp + args.to
     
     logger.info("starting exporter on port {}".format(args.port))
     prometheus_client.start_http_server(args.port)
